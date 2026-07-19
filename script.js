6b4da56999f2be46155ba7815c636a38ec9a3143
dre <100737250+frvrdre@users.noreply.github.com>
2026-07-19 18:47:05 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,512 @@
+const recipeCard = document.getElementById("recipe-card");
+const recipeFavorites = document.getElementById("recipe-favorites");
+const recipeMusthaves = document.getElementById("recipe-must-haves");
+const recipeEasyMeal = document.getElementById("recipe-easy-meal");
+
+const pomBtn = document.getElementById("pom-btn");
+const pomAudio = document.getElementById("pom-audio");
+
+let isEasyMeal = false;
+let isMusthaves = false;
+let isFavorite = false;
+
+
+const recipes = [
+  {
+    id: "hot-honey-garlic-parmesan-chicken-skewers",
+    name: "Hot Honey Garlic Parmesan Chicken Skewers",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "one-pan-chicken-and-rice",
+    name: "One Pan Chicken and Rice",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: true
+  },
+  {
+    id: "beef-tacos",
+    name: "Beef Tacos",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: true
+  },
+  {
+    id: "mac-and-cheese",
+    name: "Mac and Cheese",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: true
+  },
+  {
+    id: "chicken-garlic-baguette",
+    name: "Chicken Garlic Baguette",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "chicken-cajun-pasta",
+    name: "Chicken Cajun Pasta",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "taco-loaded-potatoes",
+    name: "Taco Loaded Potatoes",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "roast-potatoes",
+    name: "Roast Potatoes",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "seasoned-drumsticks",
+    name: "Seasoned Drumsticks",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: true
+  },
+  {
+    id: "chicken-and-shrimp-cajun-pasta",
+    name: "Chicken and Shrimp Cajun Pasta",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "chicken-and-broccoli-pasta-bake",
+    name: "Chicken and Broccoli Pasta Bake",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "red-thai-chicken-curry",
+    name: "Red Thai Chicken Curry",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: false,
+    isEasyMeal: false
+  },
+  {
+    id: "cajun-chicken-rigatoni",
+    name: "Cajun Chicken Rigatoni",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: true
+  },
+  {
+    id: "chicken-skewers-with-baby-potatoes-and-avocado",
+    name: "Chicken Skewers with Baby Potatoes and Avocado",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "chicken-sausage-pasta",
+    name: "Chicken Sausage Pasta",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "creamy-garlic-mozzarella-chicken-pasta",
+    name: "Creamy Garlic Mozzarella Chicken Pasta",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "bbq-sweet-chilli-sausage-pasta",
+    name: "BBQ Sweet Chilli Sausage Pasta",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "honey-glazed-lamb-chops",
+    name: "Honey Glazed Lamb Chops",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "southern-fried-cabbage",
+    name: "Southern Fried Cabbage",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "steamed-vegetables",
+    name: "Steamed Vegetables",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "chicken-taco",
+    name: "Chicken Taco",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "chicken-milanese",
+    name: "Chicken Milanese",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: false,
+    isEasyMeal: false
+  },
+  {
+    id: "chicken-fried-rice",
+    name: "Chicken Fried Rice",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: true
+  },
+  {
+    id: "chilli-con-carne",
+    name: "Chilli Con Carne",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "one-pan-chicken-fajita-enchiladas",
+    name: "One Pan Chicken Fajita Enchiladas",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "yellow-rice",
+    name: "Yellow Rice",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: true
+  },
+  {
+    id: "air-fryer-honey-glazed-carrots",
+    name: "Air Fryer Honey Glazed Carrots",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "cauliflower-and-broccoli-cheese",
+    name: "Cauliflower and Broccoli Cheese",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "garlic-parmesan-chicken-skewers",
+    name: "Garlic Parmesan Chicken Skewers",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "hot-honey-bbq-chicken",
+    name: "Hot Honey BBQ Chicken",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "spiced-chicken-and-rice",
+    name: "Spiced Chicken and Rice",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: true
+  },
+  {
+    id: "honey-garlic-beef-pasta",
+    name: "Honey Garlic Beef Pasta",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "korean-fried-chicken",
+    name: "Korean Fried Chicken",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "street-corn-chicken-and-rice-bowl",
+    name: "Street Corn Chicken and Rice Bowl",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: true,
+    isMusthaves: false,
+    isEasyMeal: true
+  },
+  {
+    id: "honey-chilli-beef-pasta",
+    name: "Honey Chilli Beef Pasta",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "5/5",
+    isFavorite: true,
+    isMusthaves: true,
+    isEasyMeal: false
+  },
+  {
+    id: "meat-feast-pasta",
+    name: "Meat Feast Pasta",
+    image: "/assets/images/image.png",
+    rating: "5 stars",
+    difficulty: "3/5",
+    isFavorite: false,
+    isMusthaves: true,
+    isEasyMeal: false
+  }
+];
+
+
+
+
+function renderFavCards() {
+  recipeFavorites.innerHTML = "";
+
+  recipes.forEach(function(recipe) {
+
+    if (recipe.isFavorite === true) {
+
+      recipeFavorites.innerHTML += `
+        <article class="recipe-card">
+          <a href="recipe.html?id=${recipe.id}" class="recipe-btn">
+
+            <img src="${recipe.image}" alt="Photo of ${recipe.name}">
+
+            <p class="recipe-title">${recipe.name}</p>
+
+            <div class="recipe-hover-info">
+              <p>${recipe.rating}</p>
+              <p>Difficulty: ${recipe.difficulty}</p>
+            </div>
+
+          </a>
+        </article>
+      `;
+    }
+
+  });
+}
+
+renderFavCards();
+
+function renderMustCards() {
+    recipeMusthaves.innerhtml = "";
+
+    recipes.forEach(function(recipe) {
+      if (recipe.isMusthaves === true) {
+
+        recipeMusthaves.innerHTML += `
+            <article class="recipe-card">
+          <a href="recipe.html?id=${recipe.id}" class="recipe-btn">
+
+            <img src="${recipe.image}" alt="Photo of ${recipe.name}">
+
+            <p class="recipe-title">${recipe.name}</p>
+
+            <div class="recipe-hover-info">
+              <p>${recipe.rating}</p>
+              <p>Difficulty: ${recipe.difficulty}</p>
+            </div>
+
+          </a>
+        </article>
+      `;
+      }
+    }) 
+}
+
+renderMustCards();
+
+function renderEasyCards() {
+    recipeEasyMeal.innerhtml = "";
+
+    recipes.forEach(function(recipe) {
+      if (recipe.isEasyMeal === true) {
+
+        recipeEasyMeal.innerHTML += `
+            <article class="recipe-card">
+          <a href="recipe.html?id=${recipe.id}" class="recipe-btn">
+
+            <img src="${recipe.image}" alt="Photo of ${recipe.name}">
+
+            <p class="recipe-title">${recipe.name}</p>
+
+            <div class="recipe-hover-info">
+              <p>${recipe.rating}</p>
+              <p>Difficulty: ${recipe.difficulty}</p>
+            </div>
+
+          </a>
+        </article>
+      `;
+      }
+    }) 
+}
+
+renderEasyCards();
+
+// Pom Button
+
+function pomChaos() {
+  pomAudio.play()
+  createConfetti();
+}
+
+pomBtn.addEventListener("click", pomChaos)
+
+function createConfetti() {
+  const colors = [
+    "#ff69b4",
+    "#ffd65c",
+    "#ffaacd",
+    "#ffb496",
+    "#d2b4ff",
+    "#aaebcd"
+  ];
+
+  for (let i = 0; i < 100; i++) {
+    const confetti = document.createElement("div");
+
+    confetti.classList.add("confetti");
+
+    // Random colour
+    confetti.style.backgroundColor =
+      colors[Math.floor(Math.random() * colors.length)];
+
+    // Random horizontal starting position
+    confetti.style.left = Math.random() * 100 + "vw";
+
+    // Random animation delay
+    confetti.style.animationDelay =
+      Math.random() * 0.5 + "s";
+
+    // Random falling speed
+    confetti.style.animationDuration =
+      Math.random() * 2 + 2 + "s";
+
+    document.body.appendChild(confetti);
+
+    // Remove confetti after animation
+    setTimeout(() => {
+      confetti.remove();
+    }, 5000);
+  }
+}
